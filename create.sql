8e4e15093e176d6a94a660edc6f3cd14702fa620
Vassilis Vritkas <vassilis@Vassiliss-Air.fritz.box>
2023-02-08 09:44:45 +0100

    Adding SQL-Query for Table

---
+++ create.sql
@@ -0,0 +1,29 @@
+CREATE TABLE hp_characters (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name TEXT,
+  house TEXT,
+  actor TEXT,
+);
+
+INSERT INTO hp_characters (name, house, actor)
+VALUES
+("Harry Potter", "Gryffindor", "Daniel Radcliffe"),
+("Ron Weasley", "Gryffindor", "Rupert Grint"),
+("Hermione Granger", "Gryffindor", "Emma Watson"),
+("Draco Malfoy", "Slytherin", "Tom Felton"),
+("Albus Dumbledore", "----", "Richard Harris/Michael Gambon"),
+("Severus Snape", "Slytherin", "Alan Rickman"),
+("Rubeus Hagrid", "----", "Robbie Coltrane"),
+("Sirius Black", "Gryffindor", "Gary Oldman"),
+("Luna Lovegood", "Ravenclaw", "Evanna Lynch"),
+("Neville Longbottom", "Gryffindor", "Matthew Lewis"),
+("Ginny Weasley", "Gryffindor", "Bonnie Wright"),
+("Fred Weasley", "Gryffindor", "James Phelps"),
+("George Weasley", "Gryffindor", "Oliver Phelps"),
+("Bellatrix Lestrange", "Slytherin", "Helena Bonham Carter"),
+("Voldemort", "----", "Ralph Fiennes"),
+("Dobby", "----", "Toby Jones"),
+("Griphook", "----", "Warwick Davis"),
+("Nagini", "----", "Frances de la Tour"),
+("Barty Crouch Jr.", "----", "David Tennant");
+
